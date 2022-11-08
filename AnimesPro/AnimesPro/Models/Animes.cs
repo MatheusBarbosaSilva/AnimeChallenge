fcb06fd78680699dda3391afb49c257c697ecc8f
MatheusBarbosaSilva <theubarbosa.004@gmail.com>
2022-11-07 23:41:56 -0300

    first commit

---
+++ AnimesPro/AnimesPro/Models/Animes.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AnimesPro.Models
+{
+    public class Animes
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Summary { get; set; }
+        public string Director { get; set; }
+        public bool DeletedAnime { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ AnimesPro/AnimesPro/Models/_DbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AnimesPro.Models
+{
+    public class _DbContext : DbContext
+    {
+        public _DbContext(DbContextOptions<_DbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+        public DbSet<Animes> animes { get; set; }
+    }
+}
